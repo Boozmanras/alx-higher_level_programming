58bb90701ffecd160e02905710d191257b77c784
Boozmanras <paulvictoraa@gmail.com>
2024-08-08 11:16:43 -0700

    good

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -2,20 +2,16 @@
 """Defines a class Square that can be compared based on area."""
 
 class Square:
-    """Represents a square."""
 
     def __init__(self, size=0):
-        """Initializes the square with a given size."""
         self.size = size
 
     @property
     def size(self):
-        """Gets the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square."""
         if not isinstance(value, (int, float)):
             raise TypeError("size must be a number")
         if value < 0:
@@ -23,29 +19,22 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Calculates the area of the square."""
         return self.__size ** 2
 
     def __eq__(self, other):
-        """Defines the == comparison between two squares."""
         return self.area() == other.area()
 
     def __ne__(self, other):
-        """Defines the != comparison between two squares."""
         return self.area() != other.area()
 
     def __lt__(self, other):
-        """Defines the < comparison between two squares."""
         return self.area() < other.area()
 
     def __le__(self, other):
-        """Defines the <= comparison between two squares."""
         return self.area() <= other.area()
 
     def __gt__(self, other):
-        """Defines the > comparison between two squares."""
         return self.area() > other.area()
 
     def __ge__(self, other):
-        """Defines the >= comparison between two squares."""
         return self.area() >= other.area()
